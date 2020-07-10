c3bca3bec2218a67775c59b150fd40f3ea119c8d
callor <callor@callor.com>
2020-07-10 15:29:06 +0900

    Grade 풀이

---
+++ src/com/biz/grade/domain/ScoreVO.java
@@ -0,0 +1,61 @@
+package com.biz.grade.domain;
+
+public class ScoreVO {
+
+	private String num;
+	private int kor;
+	private int eng;
+	private int math;
+	private int music;
+	
+	private int sum;
+	private float avg;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public int getMusic() {
+		return music;
+	}
+	public void setMusic(int music) {
+		this.music = music;
+	}
+	public int getSum() {
+		return sum;
+	}
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	public float getAvg() {
+		return avg;
+	}
+	public void setAvg(float avg) {
+		this.avg = avg;
+	}
+	
+	
+	
+	
+	
+}
